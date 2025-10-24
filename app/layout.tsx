d3b17ea85f4dc712254cff50f1a8cfbe7cb14b65
Giovanni Poveda <giovannipoveda51@gmail.com>
2025-10-24 14:22:50 +0200

    chore: initial setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { CssBaseline } from "@mui/material";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <CssBaseline />
         {children}
       </body>
     </html>
